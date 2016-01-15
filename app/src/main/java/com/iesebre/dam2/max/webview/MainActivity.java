34f889711b1ee6a206d28ac137bbad4bdc88d650
Max <maksymkharuk@iesebre.com>
2016-01-15 16:23:59 +0100

    First commit, created project, added WebView

---
+++ app/src/main/java/com/iesebre/dam2/max/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.iesebre.dam2.max.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://makhapps.com/");
+
+    }
+}
